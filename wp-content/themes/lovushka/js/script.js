8e035057e722601c7460bd2ae6e2352bda1157ad
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-12-23 01:13:32 +0300

    histori-40

--- wp-content/themes/lovushka/js/script.js
+++ wp-content/themes/lovushka/js/script.js
@@ -248,7 +248,7 @@ jQuery('.instruction_block').on('click', function(event) {
 
   jQuery('.protocol').css('height', jQuery('.protocol').css('width'));
   jQuery('.protocol svg').css('height', jQuery('.protocol svg').css('width'));
-  jQuery('window').on('resize', function(event) {
+  jQuery('window').resize(function(event) {
     jQuery('.protocol').css('height', jQuery('.protocol').css('width'));
     jQuery('.protocol svg').css('height', jQuery('.protocol svg').css('width'));
   });
