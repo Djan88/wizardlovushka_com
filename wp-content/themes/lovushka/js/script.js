b110b1ef9372a7cc57b72ce0367595eb5fccab3a
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-14 17:02:52 +0300

    easy_mode-32

--- wp-content/themes/lovushka/js/script.js
+++ wp-content/themes/lovushka/js/script.js
@@ -332,10 +332,12 @@ jQuery('.instruction_block').on('click', function(event) {
   jQuery('.easy_mode_item').on('click', function(event) {
     if (jQuery(this).hasClass('active')) {
       jQuery('.lovushka_speed').text(0);
-      jQuery('.protocol').css('transform', 'rotate(0deg)');
-      mode = false;
       cur_animation_val = 0;
+      count_animation = 0;
       mode_speed = 0;
+      mode = false;
+      jQuery('.protocol').css('transform', 'rotate(0deg)');
+      jQuery('.easy_mode_item').removeClass('active');
     } else {
       jQuery(this).addClass('active');
       mode = true;
