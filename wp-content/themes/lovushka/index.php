cbe4718a82439f14088f2ada77fa6105b41a6670
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-14 21:00:05 +0300

    lang-4

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -67,7 +67,7 @@
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div class="btn-group lang_block">
           <button type="button" data-lang="ru" class="btn btn-sm btn-default active btn_lang btn_lang_ru">РУС</button>
-          <button type="button" data-mode="en" data-speed="4" class="btn btn-sm btn-default btn_lang btn_lang_en">EN</button>
+          <button type="button" data-lang="en" data-speed="4" class="btn btn-sm btn-default btn_lang btn_lang_en">EN</button>
         </div>
         <div class="reg_block">
           <?php 
