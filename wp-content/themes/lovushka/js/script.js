ff92edeb45bfbc718f2d3fd7d1c0e2c133152970
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-14 13:00:48 +0300

    easy_mode-9

--- wp-content/themes/lovushka/js/script.js
+++ wp-content/themes/lovushka/js/script.js
@@ -329,6 +329,7 @@ jQuery('.instruction_block').on('click', function(event) {
     if (!jQuery(this).hasClass('active')) {
       jQuery(this).addClass('active');
       // mode = jQuery(this).data('mode');
+      console.log(jQuery(this).data('speed'))
       mode_speed = parseFloat(jQuery(this).data('speed'));
     } else {
       jQuery('.easy_mode_item').removeClass('active');
