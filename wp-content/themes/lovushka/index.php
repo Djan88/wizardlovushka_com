19f5e50d7c546bfe834f85ebf382e2b2aeb236c1
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-29 13:40:26 +0300

    kassa-1

--- wp-content/themes/lovushka/index.php
+++ wp-content/themes/lovushka/index.php
@@ -265,7 +265,23 @@
             <?php } ?>
           </div>
         <?php } else { ?>
-          <div>test</div>
+          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <h2><?php the_title(); ?></h2>
+              </div>
+              <div class="col-md-12">
+                <?php
+                the_content(__('(more...)'));
+                edit_post_link(__('Edit This'));
+                ?>
+              </div>
+            </div>
+          </div>
+          <?php endwhile; else: ?>
+            <?php _e('Sorry, no posts matched your criteria.'); ?>
+          <?php endif; ?>
         <?php } ?>
         <?php endwhile; ?>
         <?php endif; ?>
